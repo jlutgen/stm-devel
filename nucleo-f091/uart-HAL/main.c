f5ac39131b0121c5db1f1310d8b5480c306c7419
Jeff Lutgen <jlutgen@gmail.com>
2021-08-19 16:16:35 -0700

    nucleo-f091

---
+++ nucleo-f091/uart-HAL/main.c
@@ -0,0 +1,167 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  *
+  * User LED is on PA5
+  * USART2 is connected to ST-LINK USB on Nucleo board
+  ******************************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "stm32f0xx.h"
+#include "main.h"
+
+UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+
+static char msg[80];
+
+void usart_write(UART_HandleTypeDef *huart, char *s) {
+    HAL_UART_Transmit(huart, (void *)s, strlen(s), 1000);
+}
+
+void usart_read(UART_HandleTypeDef *huart, char *s, int len) {
+    for (int i = 0; i < len; i++) {
+        while (!__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE));
+        HAL_UART_Receive(huart, (void *)s, 1, 1000);
+        if (*s == '\r' || *s == '\n') {
+            *(s + 1) = '\0';
+            return;
+        }
+        ++s;
+    }
+}
+
+int main(void) {
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+
+	while(1) {
+        usart_read(&huart2, msg, 80);
+        usart_write(&huart2, "received:");
+        usart_write(&huart2, msg);
+        usart_write(&huart2, "\r\n");
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	}
+}
+
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB bus clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  // No option for MCODIV in CubeMX, so had to change this by hand
+  HAL_RCC_MCOConfig(RCC_MCO, RCC_MCO1SOURCE_SYSCLK, RCC_MCODIV_128);
+}
+
+static void MX_USART2_UART_Init(void) {
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB6 PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF0_USART1;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void Error_Handler(void) {
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_Delay(500);
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_Delay(1000);
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ uart/main.c
@@ -0,0 +1,124 @@
+
+
+/**
+  ******************************************************************************
+  * @file    main.c
+  *
+  * USART2 is connected to ST-LINK USB on Nucleo board
+  ******************************************************************************
+*/
+
+#include "stm32f0xx.h"
+#include "stm32f0xx_ll_utils.h"
+
+static char msg[80];
+
+void usart1_read(char *s, int len) {
+    for (int i = 0; i < len; i++) {
+        while((USART1->ISR & USART_ISR_RXNE) != USART_ISR_RXNE);
+        *s = (uint8_t)(USART1->RDR); /* Receive data, clear flag */
+        if (*s == '\r' || *s == '\n') {
+            *(s + 1) = '\0';
+            return;
+        }
+        ++s;
+    }
+}
+
+void usart2_read(char *s, int len) {
+    for (int i = 0; i < len; i++) {
+        while((USART2->ISR & USART_ISR_RXNE) != USART_ISR_RXNE);
+        *s = (uint8_t)(USART2->RDR); /* Receive data, clear flag */
+        if (*s == '\r' || *s == '\n') {
+            *(s + 1) = '\0';
+            return;
+        }
+        ++s;
+    }
+}
+
+void usart1_write(char *s) {
+    while (*s) {
+        USART1->TDR = *s;
+        s++;
+    }
+    while (!(USART1->ISR & USART_ISR_TC)) ;
+}
+
+void usart2_write(char *s) {
+    while (*s) {
+        USART2->TDR = *s++;
+        // Wait for data to be shifted from TDR to tx shift register
+        while (!(USART2->ISR & USART_ISR_TXE)) ;
+    }
+    // Wait for last data to be transmitted (transmission complete)
+    while (!(USART2->ISR & USART_ISR_TC)) ;
+}
+
+// user LED on A5
+int main(void) {
+	// Configure system clock for 48 MHz operation
+
+	// HSION = 1 by default (internal 8 MHz RC oscillator)
+	// and HSI is selected as system clock by default.
+	// PLL is off and not ready (i.e., is unlocked) by default.
+
+	// Set the PLL source and  multiplier (PREDIV is divide by 1 by default)
+	RCC->CFGR |= RCC_CFGR_PLLSRC_HSI_PREDIV | RCC_CFGR_PLLMUL6;
+    // Enable the PLL
+    RCC->CR |= RCC_CR_PLLON;
+    // Wait for PLL ready
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+
+	FLASH->ACR |= FLASH_ACR_LATENCY; // one wait state needed if sysclk will be over 24 MHz!!!
+    // Select PLL as system clock
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+    // Wait until PLL is switched on
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+    // Make system clock (divided by 8) available on MCO signal
+    RCC->CFGR |= RCC_CFGR_MCOPRE_DIV16;
+    RCC->CFGR |= RCC_CFGR_MCO_SYSCLK;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // Enable Port A (must do this before setting alternate function)
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // Enable USART2 on AHB
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+	GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER8) | GPIO_MODER_MODER8_1; // alternate function mode for A8
+	GPIOA->AFR[1] |= 0x00 << GPIO_AFRH_AFSEL8_Pos; // alternate function 0 on A8 is MCO (clock output)
+
+	GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER2) | GPIO_MODER_MODER2_1; // alternate function mode for A2 and A3
+	GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER3) | GPIO_MODER_MODER3_1;
+	GPIOA->AFR[0] |= 0x01 << GPIO_AFRL_AFSEL2_Pos; // alternate function 1 on A2 and A3 is USART2_TX/RX
+	GPIOA->AFR[0] |= 0x01 << GPIO_AFRL_AFSEL3_Pos;
+
+	GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODER6) | GPIO_MODER_MODER6_1; // alternate function mode for B6 and B7
+	GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODER7) | GPIO_MODER_MODER7_1;
+	GPIOB->AFR[0] |= 0x00 << GPIO_AFRL_AFSEL6_Pos; // alternate function 0 on B6 and B7 is USART1_TX/RX
+	GPIOB->AFR[0] |= 0x00 << GPIO_AFRL_AFSEL7_Pos;
+
+	GPIOA->MODER |= 0x01 << 10; // A5: output
+
+	/* (1) Oversampling by 16, 115200 baud */
+	/* (2) 8 data bit, 1 start bit, 1 stop bit, no parity */
+	USART2->BRR = 48000000 / 115200; /* (1) */
+	USART2->CR1 = USART_CR1_TE | USART_CR1_RXNEIE | USART_CR1_RE| USART_CR1_UE; /* (2) */
+
+	/* (1) Oversampling by 16, 115200 baud */
+	/* (2) 8 data bit, 1 start bit, 1 stop bit, no parity */
+	USART1->BRR = 48000000 / 115200; /* (1) */
+	USART1->CR1 = USART_CR1_TE | USART_CR1_RXNEIE | USART_CR1_RE| USART_CR1_UE; /* (2) */
+
+	LL_Init1msTick(48000000);
+
+    usart2_write("ready\r\n");
+	while(1) {
+        usart2_read(msg, 80);
+        usart2_write("received:");
+        usart2_write(msg);
+        usart2_write("\r\n");
+        GPIOA->ODR ^= 1U << 5;
+	}
+}
