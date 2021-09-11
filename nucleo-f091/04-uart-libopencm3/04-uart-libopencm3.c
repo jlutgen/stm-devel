176003ed12e4360313cc5095f656fd0c46fd97e8
Jeff Lutgen <jlutgen@gmail.com>
2021-09-10 17:37:12 -0700

    uart opencm3

--- nucleo-f091/04-uart-libopencm3/04-uart-libopencm3.c
+++ nucleo-f091/04-uart-libopencm3/04-uart-libopencm3.c
@@ -50,19 +50,19 @@ void clock_config(void) {
 
 void gpio_config(void) {
     // Must enable a peripheral's clock before writing to its registers
-    RCC_AHBENR |= RCC_AHBENR_GPIOAEN;  // Enable Port A clock
+    rcc_periph_clock_enable(RCC_GPIOA);
 
-    GPIOA_MODER |= GPIO_MODE(8, GPIO_MODE_AF);  // AF mode
-    GPIOA_AFRH |= GPIO_AFR(8 - 8, GPIO_AF0);    // AF #0 on PA8 is MCO
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8);
+    gpio_set_af(GPIOA, GPIO_AF0, GPIO8);    // AF #0 on PA8 is MCO
 
-    GPIOA_MODER |= GPIO_MODE(2, GPIO_MODE_AF);  // AF mode
-    GPIOA_AFRL |= GPIO_AFR(2, GPIO_AF1);        // AF #1 on PA2 is USART2_TX
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+    gpio_set_af(GPIOA, GPIO_AF1, GPIO2);    // AF #1 on PA2 is USART_TX
 
-    GPIOA_MODER |= GPIO_MODE(3, GPIO_MODE_AF);  // AF mode
-    GPIOA_AFRL |= GPIO_AFR(3, GPIO_AF1);        // AF #1 on PA3 is USART2_RX
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3);
+    gpio_set_af(GPIOA, GPIO_AF1, GPIO3);    // AF #1 on PA3 is USART_RX
 
     // User LED on PA5: output (push-pull by default)
-    GPIOA_MODER |= GPIO_MODE(5, GPIO_MODE_OUTPUT);
+    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 }
 
 void uart_config(void) {
@@ -70,10 +70,9 @@ void uart_config(void) {
 
     // Defaults: 8 data bits, 1 stop bit, no parity (8N1)
 
-    // Set baud rate to 115200
-    USART2_BRR = 48000000 / 115200;
-    // tx enable, rx enable, uart enable
-    USART2_CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+    usart_set_baudrate(USART2, 115200);
+    usart_set_mode(USART2, USART_MODE_TX_RX);
+    usart_enable(USART2);
 }
 
 int main(void) {
